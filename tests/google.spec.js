5376fbcc9fa5fa6b90d25f2ee9fa5f027bab3af3
Anastasiia <a.humaniuk@gmail.com>
2026-04-14 12:53:41 +0300

    test update

--- tests/google.spec.js
+++ tests/google.spec.js
@@ -3,4 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('basic test', async ({ page }) => {
   await page.goto('https://google.com');
   await expect(page).toHaveTitle(/Google/);
+  await expect(page).toHaveTitle(/Google/);
 });
