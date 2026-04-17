58910fa6c2204e218daeb2308483fc6c518f5800
Anastasiia <a.humaniuk@gmail.com>
2026-04-17 15:11:06 +0300

    added test of GET request

---
+++ tests/api/verifyRequestsToAPI.api.spec.js
@@ -0,0 +1,14 @@
+import {test, expect} from "playwright/test";
+
+test.describe('Testing of API', () => {
+    test('ID=1, Title=VerifyGetRequest', async ({ request }) => {
+        const response = await request.get('https://jsonplaceholder.typicode.com/posts');
+        expect(response.ok()).toBeTruthy();
+        const posts = await response.json();
+        expect(posts.length).toBeGreaterThan(2);
+    }
+
+    )
+
+}
+)
